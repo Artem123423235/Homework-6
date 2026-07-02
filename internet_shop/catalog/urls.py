5fec32921c85ff950854a3170466490a61c4fb9c
Artem123423235 <akarandaiev@bk.ru>
2026-07-02 15:48:03 +0300

    Delete internet_shop directory

--- internet_shop/catalog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from catalog import views
-
-urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
-    path('contacts/', views.ContactView.as_view(), name='contacts'),
-]
-
