53f0949493d4e66aeabca3a3b4b8488e6d5fe21c
Artem123423235 <akarandaiev@bk.ru>
2026-07-02 15:49:26 +0300

    Add files via upload

---
+++ internet_shop/catalog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from catalog.models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description')
+    list_filter = ('title',)
+    search_fields = ('title', 'description')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('title', 'description')
