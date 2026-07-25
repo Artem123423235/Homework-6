2400b921f07e5368360fcf40ec115659816c4766
Artem123423235 <akarandaiev@bk.ru>
2026-07-25 12:54:04 +0300

    Delete catalog directory

--- catalog/services.py
+++
@@ -1,8 +0,0 @@
-from .models import Product, Category
-
-def get_products_by_category(category_slug):
-    try:
-        category = Category.objects.get(slug=category_slug)
-        return Product.objects.filter(category=category)
-    except Category.DoesNotExist:
-        return Product.objects.none()
